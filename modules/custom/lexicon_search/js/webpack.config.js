f104d2756282cc9eedd6b0a707475e5735718301
Henrik Bak Thomassen <hbt@peytz.dk>
2018-06-11 09:49:30 +0200

    Lexicon fetch polyfill

--- modules/custom/lexicon_search/js/webpack.config.js
+++ modules/custom/lexicon_search/js/webpack.config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 
 module.exports = {
-  entry: ["babel-polyfill", "./app/index.js"],
+  entry: ["babel-polyfill", "whatwg-fetch", "./app/index.js"],
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "index.bundle.js"
